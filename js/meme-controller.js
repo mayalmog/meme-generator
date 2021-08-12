22f159590accfa19d146598bf1b6d82145c1b6fb
mayalmog <55530459+mayalmog@users.noreply.github.com>
2021-08-12 19:56:40 +0300

    main functionality

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,6 +1,7 @@
 'use strict'
 var gCanvas;
 var gCtx;
+var gCurrTxtLine;
 
 
 function onInit() {
@@ -18,26 +19,18 @@ function onInit() {
 }
 
 function onOpenEditor(num) {
-    document.querySelector('.gallery-container').classList.add("hide");
-    document.querySelector('.gallery-container').classList.remove("grid");
-    document.querySelector('.editor-container').classList.remove("hide");
+    toggleDisplay();
     var currMeme = getMeme();
     currMeme.selectedImgId = +num;
     console.log(currMeme.selectedImgId);
     drawImg(num);
-
 }
 
 
 function drawImg(id) {
-    // var currMeme = getMeme();
-    // var currImgId = currMeme.selectedImgId;
-    // console.log('drawImg', currImgId);
-
     var elImgContainer = document.querySelector('.canvas-img-container');
     elImgContainer.innerHTML = `<img class = "canvas-img" src="./images/meme-imgs (square)/${id}.jpg" alt = "img-${id}" style = "display: none;">`;
     var elImg = document.querySelector('.canvas-img');
-    console.log(elImg);
     // Naive approach:
     // there is a risk that image is not loaded yet and nothing will be drawn on canvas
 
@@ -45,55 +38,207 @@ function drawImg(id) {
 }
 
 function drawText() {
+    //TO DO: needs map to render all object texts- done
+    //TO DO: add default line placing
     var currMeme = getMeme();
+    // var currline = currMeme.selectedLineIdx;
 
-    var txt = currMeme.lines[0].txt;//map
-    var txtFontSize = currMeme.lines[0].size
-    var txtAlign = currMeme.lines[0].align;
-    var txtColor = currMeme.lines[0].color
-    //FONT
-    gCtx.font = `${txtFontSize}px Impact`;
-    // console.log(gCtx.font)
 
-    //TEXT COLORS
-    gCtx.strokeStyle = 'black'
-    gCtx.fillStyle = `${txtColor}`;
+    currMeme.lines.forEach(function (currline, idx) {
+        // var txt = currMeme.lines[currline].txt;
+        // var txtFontSize = currMeme.lines[currline].size
+        // var txtAlign = currMeme.lines[currline].align;
+        // var txtColor = currMeme.lines[currline].color
+        // var txtLineY = currMeme.lines[currline].lineY;
 
-    //TEXT ALIGN
-    switch (txtAlign) {
-        case 'center':
-            gCtx.textAlign = "center";
-            gCtx.fillText(txt, gCanvas.width / 2, gCanvas.height - gCanvas.height / 8)
-            gCtx.strokeText(txt, gCanvas.width / 2, gCanvas.height - gCanvas.height / 8)
-            break;
-        case 'left':
-            gCtx.textAlign = "left";// to the right of x
-            gCtx.fillText(txt, 10, gCanvas.height - gCanvas.height / 8)
-            gCtx.strokeText(txt, 10, gCanvas.height - gCanvas.height / 8)
-            break;
-        case 'right':
-            gCtx.textAlign = "right";//to the left of x
-            gCtx.fillText(txt, gCanvas.width - 10, gCanvas.height - gCanvas.height / 8)
-            gCtx.strokeText(txt, gCanvas.width - 10, gCanvas.height - gCanvas.height / 8)
-            break;
+        var txt = currline.txt;
+        var txtFontSize = currline.size;
+        var txtAlign = currline.align;
+        var txtColor = currline.color;
+        var txtLineY = currline.lineY;
+        var txtFont = currline.font;
+        var currLineIdx = currMeme.selectedLineIdx;
 
-    }
+        //FONT
+        gCtx.font = `${txtFontSize}px ${txtFont}`;
+
+        //TEXT COLORS
+        gCtx.strokeStyle = `${txtColor}`;
+
+        if (currline.stroke) {
+            gCtx.fillStyle = `${txtColor}`;
+        } else {
+            gCtx.fillStyle = 'white';
+        }
+
+
+        //TEXT ALIGN
+        switch (txtAlign) {
+            case 'center':
+                gCtx.textAlign = 'center';
+                if (currline.firstEdit) {
+                    gCtx.fillText(txt, gCanvas.width / 2, gCanvas.height / 8)
+                    gCtx.strokeText(txt, gCanvas.width / 2, gCanvas.height / 8)
+                    currline.lineY = getLineInitialY(currLineIdx);
+                    currline.lineX = gCanvas.width / 2;
+                    currline.firstEdit = false;
+                }
+                else {
+                    gCtx.fillText(txt, currline.lineX, txtLineY)
+                    gCtx.strokeText(txt, currline.lineX, txtLineY)
+                }
+                break;
+            case 'left':
+                gCtx.textAlign = 'left';// to the right of x
+                if (currline.firstEdit) {
+                    gCtx.fillText(txt, 10, gCanvas.height / 8)
+                    gCtx.strokeText(txt, 10, gCanvas.height / 8)
+                    currline.lineY = getLineInitialY(currLineIdx);
+                    currline.lineX = 10;
+                    currline.firstEdit = false;
+                } else {
+                    gCtx.fillText(txt, currline.lineX, txtLineY)
+                    gCtx.strokeText(txt, currline.lineX, txtLineY)
+                }
+                break;
+            case 'right':
+                gCtx.textAlign = 'right';//to the left of x
+                if (currline.firstEdit) {
+                    gCtx.fillText(txt, gCanvas.width - 10, gCanvas.height / 8)
+                    gCtx.strokeText(txt, gCanvas.width - 10, gCanvas.height / 8)
+                    currline.lineY = getLineInitialY(currLineIdx);
+                    currline.lineX = gCanvas.width - 10;
+                    currline.firstEdit = false;
+                } else {
+                    gCtx.fillText(txt, currline.lineX, txtLineY)
+                    gCtx.strokeText(txt, currline.lineX, txtLineY)
+                }
+                break;
+        }
+        if (currMeme.selectedLineIdx === idx) {
+            drawRect(gCtx.measureText(txt).width);
+        }
+    })
 
 }
 
 
 function textEditFromInput(e) {//change gMeme line text according to input
     var currMeme = getMeme();
-    var memeId = +currMeme.id;
-    console.log(memeId);
-    currMeme.lines[0].txt = e.target.value;;//map
-    // var inputTxt = document.querySelector('[name="meme-line"]').value;
+    var currline = currMeme.selectedLineIdx;
+    var memeId = +currMeme.selectedImgId;
+    //update model:
+    currMeme.lines[currline].txt = e.target.value;
 
-    console.log(currMeme.lines[0].txt)
     drawImg(memeId);
     drawText();
 }
 
+function onSelectFont() {
+    var chosenFont = document.getElementById("font").value;
+    var currMeme = getMeme();
+    var currline = currMeme.selectedLineIdx;
+    var memeId = +currMeme.selectedImgId;
+    //update model:
+    currMeme.lines[currline].font = chosenFont;
+    drawImg(memeId);
+    drawText();
+}
+
+function onSelectFontColor() {
+    var chosenFontColor = document.getElementById("color").value;
+    var currMeme = getMeme();
+    var currline = currMeme.selectedLineIdx;
+    var memeId = +currMeme.selectedImgId;
+    //update model:
+    currMeme.lines[currline].color = chosenFontColor;
+    drawImg(memeId);
+    drawText();
+}
+
+
 //event listener for input and adding current text as placeholder
+
+function onChangeFontSize(num) {
+    var currMeme = getMeme();
+    var currline = currMeme.selectedLineIdx;
+    currMeme.lines[currline].size += num;
+    var memeId = +currMeme.selectedImgId;
+    drawImg(memeId);
+    drawText();
+}
+
+function onChangeLineHeight(num) {
+    var currMeme = getMeme();
+    var currline = currMeme.selectedLineIdx;
+    currMeme.lines[currline].lineY += num;
+    var memeId = +currMeme.selectedImgId;
+
+    drawImg(memeId);
+    drawText();
+}
+
+function onAddLine() {
+    var currMeme = getMeme();
+    //add new line to object(model):
+    var newLine = { txt: 'I never eat Falafel', size: 40, align: 'center', color: 'black', lineY: 0, lineX: 0, font: 'Impact', firstEdit: true, stroke: false };
+    currMeme.lines.push(newLine);
+    //change line index to new line:
+    currMeme.selectedLineIdx++;
+}
+
+function onSwitchLines() {
+    var currMeme = getMeme();
+    var memeId = +currMeme.selectedImgId;
+    currMeme.selectedLineIdx++;
+    if (currMeme.selectedLineIdx >= currMeme.lines.length) {
+        currMeme.selectedLineIdx = 0;
+    }
+    ///TO DO: focus- done. requires rendering:
+    drawImg(memeId);
+    drawText();
+    ///TO DO: insert current line text as placeholder
+
+}
+
+function onChangeTxtAlign(chosenAlign) {
+    var currMeme = getMeme();
+    var memeId = +currMeme.selectedImgId;
+    var currLine = currMeme.selectedLineIdx;
+    currMeme.lines[currLine].align = chosenAlign;
+    console.log('currLine: ' + currLine + '. chosen align: ' + currMeme.lines[currLine].align);
+    //render DOM:
+    drawImg(memeId);
+    drawText();
+
+}
+
+function onDeleteLine() {
+    var currMeme = getMeme();
+    var memeId = +currMeme.selectedImgId;
+    var currLine = currMeme.selectedLineIdx;
+    currMeme.lines.splice(currLine, 1);
+    drawImg(memeId);
+    drawText();
+}
+
+function downloadImg(elLink) {
+    var imgContent = gCanvas.toDataURL('image/jpeg');
+    elLink.href = imgContent;
+}
+
+function toggleStroke() {
+    var currMeme = getMeme();
+    var memeId = +currMeme.selectedImgId;
+    var currLine = currMeme.selectedLineIdx;
+    if (currMeme.lines[currLine].stroke) {
+        currMeme.lines[currLine].stroke = false;
+    } else {
+        currMeme.lines[currLine].stroke = true;
+    }
+    drawImg(memeId);
+    drawText();
+}
+
 
 
--- js/meme-service.js
+++ js/meme-service.js
@@ -5,7 +5,7 @@ var gKeywords = { 'happy': 12, 'funny puk': 1 };
 var gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
-    lines: [{ txt: 'I never eat Falafel', size: 40, align: 'right', color: 'white' }]
+    lines: [{ txt: 'I never eat Falafel', size: 40, align: 'center', color: 'black', lineY: 0, lineX: 0, font: 'Impact', firstEdit: true, stroke: false }]
 }
 
 var gImgs = [
@@ -15,5 +15,59 @@ var gImgs = [
 
 function getMeme() {
     return gMeme;
+}
+
+function toggleDisplay() {
+    document.querySelector('.gallery-container').classList.toggle("hide");
+    document.querySelector('.gallery-container').classList.toggle("grid");
+    document.querySelector('.editor-container').classList.toggle("hide");
+}
+
+// function drawPoint(x, y) {
+//     gCtx.beginPath();
+//     gCtx.arc(x, y, 10, 0, 2 * Math.PI);
+//     gCtx.fillStyle = 'green';
+//     gCtx.fill();
+//     gCtx.stroke();
+// }
+
+function drawRect(width) {
+    var currLine = gMeme.lines[gMeme.selectedLineIdx];
+    var height = -currLine.size - 10;
+    var currLineAlign = currLine.align;
+    var widthForRect = width + 20;
+
+    switch (currLineAlign) {
+        case 'center':
+            var x = currLine.lineX - 0.5 * width - 10;
+            var y = currLine.lineY + 10;
+            break;
+        case 'left':
+            var x = currLine.lineX - 10;
+            var y = currLine.lineY + 10;
+            break;
+        case 'right':
+            var x = currLine.lineX - width - 10;
+            var y = currLine.lineY + 10;
+            break;
+
+    }
+
+    gCtx.beginPath();
+    gCtx.rect(x, y, widthForRect, height);
+    gCtx.strokeStyle = 'white';
+    gCtx.lineWidth = 2;
+    gCtx.stroke();
+}
+
+function getLineInitialY(lineIdx) {
+
+    if (lineIdx === 0) {
+        return gCanvas.height / 8;
+    } else if (lineIdx === 1) {
+        return gCanvas.height - gCanvas.height / 8;
+    } else {
+        return gCanvas.height / 2;
+    }
 }
 
